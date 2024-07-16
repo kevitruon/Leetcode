150a024b848bc3cf14f56bed1d8d072951091b88
Kevin Truong <146457163+kevitruon@users.noreply.github.com>
2024-07-16 16:43:48 -0700

    Time: 52 ms (31.89%), Space: 16.4 MB (77.58%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.py
@@ -0,0 +1,26 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        conversion = {
+            "M":1000,
+            "CM":900,
+            "D":500,
+            "CD":400,
+            "C":100,
+            "XC":90,
+            "L":50,
+            "XL":40,
+            "X":10,
+            "IX":9,
+            "V":5,
+            "IV":4,
+            "I":1
+        }
+        res= ""
+        for k,v in conversion.items():
+            while num >= v:
+                res+=k
+                num-=v
+        return res
+
+
+        
